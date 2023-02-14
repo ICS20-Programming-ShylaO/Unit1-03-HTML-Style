e70e3005065953ebe6b87cc0b6b9e13b0ec088ac
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-02-14 23:21:28 +0000

    updated index, added cities file, images folder with image files, fixed any errors with linters

--- replit.nix
+++ replit.nix
@@ -1,5 +1,7 @@
 { pkgs }: {
   deps = [
+    pkgs.timetrap.out#myHeader {
+    pkgs.nodejs
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
